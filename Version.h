9297cea31a95d9b370725bca1afaad12f0cbc76b
Valery <tor-tech@mail.ru>
2024-02-28 15:24:14 +0300

    Update Version.h

--- Version.h
+++ Version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,8,1,0
-#define VER_FILEVERSION_STR         "1.8.1.0\0"
+#define VER_FILEVERSION             1,8,2,0
+#define VER_FILEVERSION_STR         "1.8.2.0\0"
 
-#define VER_PRODUCTVERSION          1,8,1,0
-#define VER_PRODUCTVERSION_STR      "1.8.1\0"
+#define VER_PRODUCTVERSION          1,8,2,0
+#define VER_PRODUCTVERSION_STR      "1.8.2\0"
 
 #define VER_COMPANYNAME_STR         "IAPP of BSU"
 #define VER_FILEDESCRIPTION_STR     "spectrasynthesizer"
